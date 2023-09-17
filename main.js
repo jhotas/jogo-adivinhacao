ac7c9e0fdb35c3a2e5a72dc6a86ca19292ee6006
Jean Oliveira <jcwilbertt@gmail.com>
2023-09-17 02:41:10 +0100

    reset

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
 const randomNumber = Math.round(Math.random() * 10)
 let attempts = 1
 
-function handleClick(event) {
+// Função callback
+function handleTryClick(event) {
   event.preventDefault()
   const inputNumber = document.querySelector("#inputNumber")
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
-    document.querySelector(".screen2 h2").innerText = `Acertou em ${attempts} tentativas!`
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
+    screen2.querySelector("h2").innerText = `Acertou em ${attempts} tentativas!`
   }
-  
+
+  inputNumber.value = ""
   attempts++
 }
+
+// Eventos
+const btnTry =  document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function () {
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+  attempts = 1
+})
